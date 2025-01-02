ad7ee40d498afd8c779b997501a837c3b2acf26b
N.I <iworksWeb@gmail.com>
2025-01-02 22:39:55 +0900

    state management without useState

--- src/lessons/Lesson1/Lesson1_1.tsx
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -1,9 +1,20 @@
 const Lesson1_1 = () => {
+  let age = 0;
+  const handleClick = () => {
+    age = age + 1;
+    console.log(age);
+  };
+
   return (
     <div>
       <input type="text" />
-      <button className="border p-2 rounded-md bg-red-100">Add Age</button>
-      <p></p>
+      <button
+        className="border p-2 rounded-md bg-red-100"
+        onClick={handleClick}
+      >
+        Add Age
+      </button>
+      <p>You are {age}</p>
     </div>
   );
 };
