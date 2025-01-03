199ea791cf265b7b8638e1836cbd018a3d92cd59
N.I <iworksWeb@gmail.com>
2025-01-03 14:30:52 +0900

    Update State of Objects and arrays

--- src/lessons/Lesson1/Lesson1_2.tsx
+++ src/lessons/Lesson1/Lesson1_2.tsx
@@ -1,23 +1,64 @@
+import { ChangeEvent, useState } from "react";
+
 const Lesson1_2 = () => {
+  const [form, setForm] = useState({
+    firstName: "Iwa",
+    lastName: "Naobe",
+    email: "naobe@gmail.com",
+  });
+
   return (
     <div>
       <div className="flex mb-5">
         <label>
           First Name:
-          <input type="text" className="border border-slate-500" />
+          <input
+            type="text"
+            className="border border-slate-500"
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setForm({
+                firstName: e.target.value,
+                lastName: form.lastName,
+                email: form.email,
+              })
+            }
+          />
         </label>
         <label>
           Last Name:
-          <input type="text" className="border border-slate-500" />
+          <input
+            type="text"
+            className="border border-slate-500"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setForm({
+                firstName: form.firstName,
+                lastName: e.target.value,
+                email: form.email,
+              });
+            }}
+          />
         </label>
         <label>
           Email:
-          <input type="text" className="border border-slate-500" />
+          <input
+            type="text"
+            className="border border-slate-500"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setForm({
+                firstName: form.firstName,
+                lastName: form.lastName,
+                email: e.target.value,
+              });
+            }}
+          />
         </label>
       </div>
       <p>
+        {form.firstName}
         <br />
+        {form.lastName}
         <br />
+        {form.email}
       </p>
     </div>
   );
