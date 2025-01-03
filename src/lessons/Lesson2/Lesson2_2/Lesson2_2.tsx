ad2921e85dc27cdb6cc4c9ef9362122b56930f8d
N.I <iworksWeb@gmail.com>
2025-01-03 23:50:42 +0900

    Data fetching using useEffect

--- src/lessons/Lesson2/Lesson2_2/Lesson2_2.tsx
+++ src/lessons/Lesson2/Lesson2_2/Lesson2_2.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+import { fetchBio } from "./fetchBio";
+
 const Lesson2_2 = () => {
+  const [person, setPerson] = useState<string>("naobe");
+
+  useEffect(() => {
+    const startFetching = async () => {
+      const resoponse = await fetchBio(person);
+      console.log(resoponse);
+    };
+    startFetching();
+  }, []);
+
   return (
     <div>
-      <select>
+      <select onChange={(e) => setPerson(e.target.value)} value={person}>
         <option value="ShinCode">ShinCode</option>
         <option value="TestUser">TestUser</option>
         <option value="SampleUser">SampleUser</option>
